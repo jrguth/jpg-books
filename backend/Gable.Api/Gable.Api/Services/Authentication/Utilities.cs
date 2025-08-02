342123f9ddae3936d41797a4f27c0683363a3675
jrguth <jacobguth96@gmail.com>
2025-08-02 16:55:13 -0500

    add get userclaims utility

---
+++ backend/Gable.Api/Gable.Api/Services/Authentication/Utilities.cs
@@ -0,0 +1,18 @@
+using System.Security.Claims;
+using Gable.Api.Db.Models;
+
+namespace Gable.Api.Services.Authentication;
+
+public static class Utilities
+{
+    public static IEnumerable<Claim> GetUserClaims(this User user)
+    {
+        return new List<Claim>
+        {
+            new Claim("email", user.Email),
+            new Claim("name", user.Name ?? ""),
+            new Claim("userId", user.Id.ToString()),
+            new Claim("userName", user.UserName ?? ""),
+        };
+    }
+}
